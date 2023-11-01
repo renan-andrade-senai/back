e3a5e31ecdbfcedccb842871fa1c24c976ebd3f3
NicholasFerreiraMoura <nicholas_moura@estudante.sesisenai.org.br>
2023-11-01 15:29:23 -0300

    start

---
+++ src/main/java/com/senai/br/modulo_avaliacao/ModuloAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.br.modulo_avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/br/modulo_avaliacao/ModuloAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.br.modulo_avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
